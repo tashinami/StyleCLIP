4860d5f81b5f59d88f0aa274346a29ec7d3ed81c
tashinami <undermmusic@gmail.com>
2021-10-28 16:22:32 +0900

    tqdm

--- image2latent.py
+++ image2latent.py
@@ -70,66 +70,67 @@ w_opt = torch.tensor(w_avg, dtype=torch.float32, device=device, requires_grad=Tr
 w_out = torch.zeros([num_steps] + list(w_opt.shape[1:]), dtype=torch.float32, device=device)
 optimizer = torch.optim.Adam([w_opt] + list(noise_bufs.values()), betas=(0.9, 0.999), lr=initial_learning_rate)
 
-
-
 # Init noise.
 for buf in noise_bufs.values():
     buf[:] = torch.randn_like(buf)
     buf.requires_grad = True
 
-for step in range(num_steps):
-    # Learning rate schedule.
-    t = step / num_steps
-    w_noise_scale = w_std * initial_noise_factor * max(0.0, 1.0 - t / noise_ramp_length) ** 2
-    lr_ramp = min(1.0, (1.0 - t) / lr_rampdown_length)
-    lr_ramp = 0.5 - 0.5 * np.cos(lr_ramp * np.pi)
-    lr_ramp = lr_ramp * min(1.0, t / lr_rampup_length)
-    lr = initial_learning_rate * lr_ramp
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+with tqdm(total=num_steps, unit="itr") as pbar:
+    for step in range(num_steps):
+        # Learning rate schedule.
+        t = step / num_steps
+        w_noise_scale = w_std * initial_noise_factor * max(0.0, 1.0 - t / noise_ramp_length) ** 2
+        lr_ramp = min(1.0, (1.0 - t) / lr_rampdown_length)
+        lr_ramp = 0.5 - 0.5 * np.cos(lr_ramp * np.pi)
+        lr_ramp = lr_ramp * min(1.0, t / lr_rampup_length)
+        lr = initial_learning_rate * lr_ramp
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
 
-    # Synth images from opt_w.
-    w_noise = torch.randn_like(w_opt) * w_noise_scale
-    ws = (w_opt + w_noise).repeat([1, generator.mapping.num_ws, 1])
-    synth_images = generator.synthesis(ws, noise_mode='const')
+        # Synth images from opt_w.
+        w_noise = torch.randn_like(w_opt) * w_noise_scale
+        ws = (w_opt + w_noise).repeat([1, generator.mapping.num_ws, 1])
+        synth_images = generator.synthesis(ws, noise_mode='const')
 
-    # Downsample image to 256x256 if it's larger than that. VGG was built for 224x224 images.
-    synth_images = (synth_images + 1) * (255/2)
-    if synth_images.shape[2] > 256:
-        synth_images = F.interpolate(synth_images, size=(256, 256), mode='area')
+        # Downsample image to 256x256 if it's larger than that. VGG was built for 224x224 images.
+        synth_images = (synth_images + 1) * (255/2)
+        if synth_images.shape[2] > 256:
+            synth_images = F.interpolate(synth_images, size=(256, 256), mode='area')
 
-    # Features for synth images.
-    synth_features = vgg16(synth_images, resize_images=False, return_lpips=True)
-    dist = (target_features - synth_features).square().sum()
+        # Features for synth images.
+        synth_features = vgg16(synth_images, resize_images=False, return_lpips=True)
+        dist = (target_features - synth_features).square().sum()
 
-    # Noise regularization.
-    reg_loss = 0.0
-    for v in noise_bufs.values():
-        noise = v[None,None,:,:] # must be [1,1,H,W] for F.avg_pool2d()
-        while True:
-            reg_loss += (noise*torch.roll(noise, shifts=1, dims=3)).mean()**2
-            reg_loss += (noise*torch.roll(noise, shifts=1, dims=2)).mean()**2
-            if noise.shape[2] <= 8:
-                break
-            noise = F.avg_pool2d(noise, kernel_size=2)
-    loss = dist + reg_loss * regularize_noise_weight
+        # Noise regularization.
+        reg_loss = 0.0
+        for v in noise_bufs.values():
+            noise = v[None,None,:,:] # must be [1,1,H,W] for F.avg_pool2d()
+            while True:
+                reg_loss += (noise*torch.roll(noise, shifts=1, dims=3)).mean()**2
+                reg_loss += (noise*torch.roll(noise, shifts=1, dims=2)).mean()**2
+                if noise.shape[2] <= 8:
+                    break
+                noise = F.avg_pool2d(noise, kernel_size=2)
+        loss = dist + reg_loss * regularize_noise_weight
 
-    # Step
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
-    print(f'step {step+1:>4d}/{num_steps}: dist {dist:<4.2f} loss {float(loss):<5.2f}')
+        # Step
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
 
-    # Save projected W for each optimization step.
-    w_out[step] = w_opt.detach()[0]
+        pbar.set_postfix({"dist":dist.item(), "loss" : loss.item()})
+        pbar.update(1)
 
-    # Normalize noise.
-    with torch.no_grad():
-        for buf in noise_bufs.values():
-            buf -= buf.mean()
-            buf *= buf.square().mean().rsqrt()
+        # Save projected W for each optimization step.
+        w_out[step] = w_opt.detach()[0]
 
-    results =  w_out.repeat([1, generator.mapping.num_ws, 1])
+        # Normalize noise.
+        with torch.no_grad():
+            for buf in noise_bufs.values():
+                buf -= buf.mean()
+                buf *= buf.square().mean().rsqrt()
+
+        results =  w_out.repeat([1, generator.mapping.num_ws, 1])
 
 
 # Save final projected frame and W vector.
